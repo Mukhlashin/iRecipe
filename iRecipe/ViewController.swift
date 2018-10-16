6b75863e435f53f72e7e7640d1c113d20144f5a2
Abdullah Mukhlasin <abdullahmukhlasinrpld@mukhlashs-macbook-pro.local>
2018-10-16 20:43:05 +0700

    Initial Commit

---
+++ iRecipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iRecipe
+//
+//  Created by Abdullah Mukhlasin (RPL D ) on 16/10/18.
+//  Copyright © 2018 Culdesac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
